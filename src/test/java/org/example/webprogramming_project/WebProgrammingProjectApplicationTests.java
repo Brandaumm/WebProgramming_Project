773ae8e790413edcd2f219c21434a32902f09945
Brandaumm <nathan.victor.brandao@gmail.com>
2024-07-26 12:11:17 -0300

    Starting Project

---
+++ src/test/java/org/example/webprogramming_project/WebProgrammingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webprogramming_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProgrammingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
